7c7ce80323b1e6ab5a3542dbf2c5af82b8ddf3d2
daisuke <dai175dai@gmail.com>
2021-05-15 11:40:45 +0900

    dev

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -18,13 +18,30 @@ router.get('/', async (req: Request, res: Response) => {
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const item = await FeedItem.findByPk(id);
+    if (!item) {
+        res.send(404);
+    }
+    res.send(item);
+});
 
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
         //@TODO try it yourself
-        res.send(500).send("not implemented")
+        const { id } = req.params;
+        const { caption, url } = req.body;
+
+        await FeedItem.update(
+            { caption, url },
+            { where: { id: id } }
+        );
+
+        let item = await FeedItem.findByPk(id);
+        res.send(item);
 });
 
 
