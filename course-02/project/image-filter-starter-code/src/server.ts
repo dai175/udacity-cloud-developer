780833abc1a55839f38820a00465a872c127b78f
daisuke <dai175dai@gmail.com>
2021-05-16 22:47:34 +0900

    review

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -28,8 +28,8 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-  app.get( "/filteredimage", async ( req, res ) => {
-    const { image_url } = req.query;
+  app.get( "/filteredimage", async ( req: express.Request, res: express.Response ) => {
+    const image_url = req.query;
 
     if (!image_url) {
       return res.status(400).send("image_url is required");
